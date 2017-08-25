3da6ce86126e54d8214350ba3f07dbc570a545c4
Pawel Rabinovic <trueone777@googlemail.com>
2017-08-25 14:30:35 +0200

    Created assignment!

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,10 +2,16 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
+    TextView mTextView;
+    Button mTopButton, mBottomButton;
+    int myStoryIndex = 1;
 
 
     @Override
@@ -15,16 +21,89 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        mTextView = (TextView) findViewById(R.id.storyTextView);
+        mTopButton = (Button) findViewById(R.id.buttonTop);
+        mBottomButton = (Button) findViewById(R.id.buttonBottom);
 
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+        mTopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                if (myStoryIndex == 1) {
+                    stepTo(3);
+                    myStoryIndex = 2;
+                } else if (myStoryIndex == 2) {
+                    stepTo(6);
+                } else if (myStoryIndex == 3) {
+                    stepTo(3);
+                    myStoryIndex = 4;
+                } else if (myStoryIndex == 4) {
+                    stepTo(6);
+                }
+
+            }
+        });
 
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+        mBottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                if (myStoryIndex == 1) {
+                    stepTo(2);
+                    myStoryIndex = 3;
+                } else if (myStoryIndex == 2) {
+                    stepTo(5);
+                } else if (myStoryIndex == 3) {
+                    stepTo(4);
+                } else if (myStoryIndex == 4) {
+                    stepTo(5);
+                }
+
+            }
+        });
 
     }
+
+    public void stepTo(int state) {
+
+        if (state == 1) {
+            mTextView.setText(R.string.T1_Story);
+            mTopButton.setText(R.string.T1_Ans1);
+            mBottomButton.setText(R.string.T1_Ans2);
+        } else if (state == 2) {
+            mTextView.setText(R.string.T2_Story);
+            mTopButton.setText(R.string.T2_Ans1);
+            mBottomButton.setText(R.string.T2_Ans2);
+        } else if ( state == 3) {
+            mTextView.setText(R.string.T3_Story);
+            mTopButton.setText(R.string.T3_Ans1);
+            mBottomButton.setText(R.string.T3_Ans2);
+        } else if (state == 4) {
+            mTextView.setText(R.string.T4_End);
+            mTopButton.setVisibility(View.GONE);
+            mBottomButton.setVisibility(View.GONE);
+        } else if (state == 5) {
+            mTextView.setText(R.string.T5_End);
+            mTopButton.setVisibility(View.GONE);
+            mBottomButton.setVisibility(View.GONE);
+        } else if ( state == 6) {
+            mTextView.setText(R.string.T6_End);
+            mTopButton.setVisibility(View.GONE);
+            mBottomButton.setVisibility(View.GONE);
+        }
+
+    }
+
+
+
+
+
+
+
 }
